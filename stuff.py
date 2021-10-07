a8c46a6714154923d8f195277546cb9470891610
Ben-Littell <89919066+Ben-Littell@users.noreply.github.com>
2021-10-07 09:34:18 -0400

    did parts 1 and 2 and started 3

--- stuff.py
+++ stuff.py
@@ -6,30 +6,51 @@ import csv
 # In Main each choice should rely on a function that will return the correct info
 ########################################################################################################################
 def open_file():
+    company_db_dict = {}
+    name = 'row'
+    numb = 1
     with open('Company_DB - Sheet1.csv') as db:
         company_db = csv.reader(db)
-        company_db2 = []
         for row in company_db:
-            company_db2.append(row)
-        return company_db2
+            company_db_dict.update({name + str(numb): row})
+            numb += 1
+    return company_db_dict
 
 
 def get_age():
-    dict_age = {}
-    with open('Company_DB - Sheet1.csv') as db:
-        company_db = csv.reader(db)
-        for row in company_db:
-            dict_age.update({row[3]: row})
-    print(dict_age)
+    dict_age = open_file()
+    key_list = []
     start_age = input('Enter start age: ')
     end_age = input('Enter end age: ')
-    for numb in range(int(start_age), int(end_age)+1):
-        pass
+    for numb in range(int(start_age), int(end_age) + 1):
+        for key in dict_age:
+            key_val = dict_age.get(key)
+            if key_val[3] == str(numb):
+                key_list.append(key)
+    for key in key_list:
+        print(dict_age[key])
 
+
+def get_gender():
+    dict_gender = open_file()
+    key_list = []
+    gender = input('Enter M for male, F for female: ')
+    for key in dict_gender:
+        key_val = dict_gender.get(key)
+        if key_val[2] == gender.upper():
+            key_list.append(key)
+    for key in key_list:
+        print(dict_gender[key])
+
+
+def get_salary():
+    dict_salary = open_file()
+    key_list = []
+    salary_start = input('Enter salary start: ')
+    salary_end = input('Enter salary end: ')
 
 
 def main():
-    file = open_file()
     run = True
     while run:
         prompt = input('What kind of information do you wish to extract :\n'
@@ -43,7 +64,8 @@ def main():
             get_age()
             run = False
         elif prompt == '2':
-            pass
+            get_gender()
+            run = False
         elif prompt == '3':
             pass
         elif prompt == '4':
